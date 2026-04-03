e962ef3781d297169284b5944028a9049cbbcfd2
Jashvarthini Rajesh <jashvarthinirajesh@gmail.com>
2026-04-03 18:48:55 +0530

    Port correction

--- server.py
+++ server.py
@@ -93,5 +93,4 @@ async def login(data: AuthRequest):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
